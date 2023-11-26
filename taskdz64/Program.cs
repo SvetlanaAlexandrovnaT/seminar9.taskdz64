d08918a42241ca3624ee7260617185d8e1266915
Svelana <svettigr@mail.ru>
2023-11-26 15:06:27 +0700

    seminar9.taskdz64

--- taskdz64/Program.cs
+++ taskdz64/Program.cs
@@ -18,22 +18,3 @@ void RoadToN(int number)
 
 int N = InputNumber("Введите число: ");
 RoadToN(N);
-
-// Console.Write("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// int count = 1;
-// NaturalToLow(number, count);
-
-
-// void NaturalToLow(int n, int count)
-// {
-//     if (count > n)
-//     {
-//         return;
-//     }
-//     else
-//     {
-//         NaturalToLow(n, count + 1);
-//         Console.Write(count + " ");
-//     }
-// }
